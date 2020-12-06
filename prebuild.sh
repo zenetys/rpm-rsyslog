c8520285e05b88cc733ecba8fa5b45c58e22b916
Julien Thomas <jthomas@zenetys.com>
2020-12-06 19:41:16 +0100

    BUILD: Fix el6 build, use archive repositories since EOL

--- prebuild.sh
+++ prebuild.sh
@@ -3,7 +3,10 @@
 case "$DIST" in
     el6)
         # EPEL6: libnet
-        build_dl "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$DIST_VERSION.noarch.rpm"
-        rpm -Uvh "$CACHEDIR/epel-release-latest-$DIST_VERSION.noarch.rpm"
+        build_dl https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/epel-release-6-8.noarch.rpm
+        rpm -Uvh "$CACHEDIR/epel-release-6-8.noarch.rpm"
+        # el6 EOL, use archive repositories
+        sed -i -re 's,mirror\.centos\.org,vault.centos.org,; s,^(mirrorlist),#\1,; s,^#(baseurl),\1,' \
+            /etc/yum.repos.d/CentOS-Base.repo
         ;;
 esac
